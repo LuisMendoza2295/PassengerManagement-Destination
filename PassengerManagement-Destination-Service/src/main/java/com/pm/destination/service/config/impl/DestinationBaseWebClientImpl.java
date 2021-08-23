6e5e594b80bfac6d9071c5b5ddbb00c15edbdfdb
luismendoza2295 <luismendoza2295@gmail.com>
2021-08-23 14:35:31 -0500

    sleuth integration

--- PassengerManagement-Destination-Service/src/main/java/com/pm/destination/service/config/impl/DestinationBaseWebClientImpl.java
+++ PassengerManagement-Destination-Service/src/main/java/com/pm/destination/service/config/impl/DestinationBaseWebClientImpl.java
@@ -1,7 +1,7 @@
 package com.pm.destination.service.config.impl;
 
 import com.pm.destination.service.config.DestinationBaseWebClient;
-import com.pm.destination.service.util.MDCUtil;
+import com.pm.destination.service.exception.ExternalServiceException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.ParameterizedTypeReference;
@@ -23,8 +23,7 @@ public class DestinationBaseWebClientImpl implements DestinationBaseWebClient {
     @Override
     public <T> Mono<T> retrieveMono(HttpMethod httpMethod, URI uri, HttpHeaders httpHeaders, Object body, Class<T> tClass) {
         return Mono.deferWithContext(context -> retrieve(httpMethod, uri, httpHeaders, body)
-                .flatMap(response -> response.bodyToMono(tClass))
-                .doOnError(throwable -> log.error("ERROR ON SERVICE CALL")));
+                .flatMap(response -> response.bodyToMono(tClass)));
     }
 
     @Override
@@ -34,13 +33,8 @@ public class DestinationBaseWebClientImpl implements DestinationBaseWebClient {
 
     @Override
     public <T> Flux<T> retrieveFlux(HttpMethod httpMethod, URI uri, HttpHeaders httpHeaders, Object body, Class<T> tClass) {
-        return Flux.deferWithContext(ctx -> {
-            httpHeaders.add(MDCUtil.MDC_ID, ctx.get("CONTEXT_KEY").toString());
-            return retrieve(httpMethod, uri, httpHeaders, body)
-                    .flatMapMany(clientResponse -> clientResponse.bodyToFlux(tClass))
-                    .doOnEach(MDCUtil.logOnComplete(data -> log.info("COMPLETED SERVICE CALL {} {}", httpMethod, uri)))
-                    .doOnEach(MDCUtil.logOnError(error -> log.error("ERROR ON SERVICE CALL {} {}", httpMethod, uri)));
-        });
+        return retrieve(httpMethod, uri, httpHeaders, body)
+                .flatMapMany(clientResponse -> clientResponse.bodyToFlux(tClass));
     }
 
     @Override
@@ -55,8 +49,14 @@ public class DestinationBaseWebClientImpl implements DestinationBaseWebClient {
                 .headers(headers -> headers.addAll(httpHeaders))
                 .bodyValue(body)
                 .exchange()
-//                .onErrorResume(throwable -> wrapException(httpMethod, uri, throwable))
-//                .flatMap(clientResponse -> checkResponse(httpMethod, uri, body, clientResponse))
+                .flatMap(clientResponse -> {
+                    if(clientResponse.statusCode().isError()) {
+                        return Mono.error(new ExternalServiceException(clientResponse.statusCode()));
+                    }
+                    return Mono.just(clientResponse);
+                })
+                .doOnSuccess(clientResponse -> log.info("COMPLETED SERVICE CALL {} {}", httpMethod, uri))
+                .doOnError(throwable -> log.error("ERROR ON SERVICE CALL {} {}", httpMethod, uri))
                 .name(httpMethod + " - " + uri.getPath())
                 .metrics();
     }
--- PassengerManagement-Destination-Webapp/src/main/java/com/pm/destination/webapp/config/RequestIDFilter.java
+++ PassengerManagement-Destination-Webapp/src/main/java/com/pm/destination/webapp/config/RequestIDFilter.java
@@ -1,15 +1,13 @@
 package com.pm.destination.webapp.config;
 
-import com.pm.destination.service.util.MDCUtil;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
 import reactor.core.publisher.Mono;
-import reactor.util.context.Context;
 
 @Slf4j
 @Configuration
@@ -18,12 +16,8 @@ public class RequestIDFilter implements WebFilter {
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
-        ServerHttpRequest request = exchange.getRequest();
-        String requestID = MDCUtil.getRequestId(request.getHeaders());
-
         return chain.filter(exchange)
-                .doOnEach(MDCUtil.logOnComplete(data -> log.info("COMPLETED {} {}", request.getMethod(), request.getURI())))
-                .doOnEach(MDCUtil.logOnError(error -> log.error("ERROR ON {} {}", request.getMethod(), request.getURI())))
-                .subscriberContext(Context.of("CONTEXT_KEY", requestID));
+                .doOnSuccess(unused -> log.info("COMPLETED {}", MDC.get("X-B3-TraceId")))
+                .doOnError(throwable -> log.info("ERROR {} {}", throwable.getMessage(), MDC.get("X-B3-TraceId")));
     }
 }
